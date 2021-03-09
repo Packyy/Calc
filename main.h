3154fcfe7e2ffda38cd550dfc1ab2ab9504993e1
Packyy <76876624+Packyy@users.noreply.github.com>
2021-03-09 22:19:17 +0600

    Create main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <Windows.h>
+
+char de;
+float a;
+float b;
+float result;
+	
+inline float sum(float a, float b);
+inline float min(float a, float b);
+inline float del(float a, float b);
+inline float umn(float a, float b);
